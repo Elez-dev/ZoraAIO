7b5d1f78d0fd1f097615a23e3791194fc0090d0c
Maikl iz University <107795811+Elez-dev@users.noreply.github.com>
2024-01-12 00:48:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -271,7 +271,7 @@ if __name__ == '__main__':
             time.sleep(0.1)
             act = int(input('Choose an action: '))
 
-            if act in range(1, 21):
+            if act in range(1, 22):
                 break
 
         worker = Worker(act)
